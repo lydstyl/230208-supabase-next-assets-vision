4df807c1b66e3c30816f38eac62636f4125ac68a
Gabriel Brun <lydstyl@gmail.com>
2023-02-21 23:55:59 +0100

    Add and delete a category.

--- pages/categories.tsx
+++ pages/categories.tsx
@@ -1,12 +1,66 @@
 import useCategories from "@/hooks/useCategories"
+import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react"
+import { Category } from "@/utils/types"
 
 export default function Categories() {
-    const { data, loading, error } = useCategories()
+    const { data, loading, error } = useCategories() // to get categories
+
+    const session = useSession()
+    const supabase = useSupabaseClient()
+
+    const handleClickAddCategory = () => {
+        ;(async function () {
+            try {
+                const newCategory = {
+                    name: "new cat",
+                    user_id: session?.user.id,
+                }
+
+                const { data, error } = await supabase
+                    .from("categories")
+                    .insert(newCategory)
+
+                if (error) {
+                    console.error(error)
+                    return
+                }
+                if (data) {
+                    console.log(`gb🚀 ~ data:`, data)
+                }
+            } catch (error) {
+                console.log(`gb🚀 ~ error`, error)
+            }
+        })()
+    }
+
+    const handleClickDeleteCategory = () => {
+        ;(async function () {
+            try {
+                const { error } = await supabase
+                    .from("categories")
+                    .delete()
+                    // .eq('id', 11)
+                    .eq("name", "new cat")
+
+                if (error) {
+                    console.error(error)
+                    return
+                }
+                if (data) {
+                    console.log(`gb🚀 ~ data:`, data)
+                }
+            } catch (error) {
+                console.log(`gb🚀 ~ error`, error)
+            }
+        })()
+    }
 
     return (
         <div>
             <h2>Add a category</h2>
             <input type="text" name="name" />
+            <button onClick={handleClickAddCategory}>add category</button>
+            <button onClick={handleClickDeleteCategory}>delete category</button>
             <h2>Liste des categories</h2>
             {loading ? (
                 <p>Loading...</p>
